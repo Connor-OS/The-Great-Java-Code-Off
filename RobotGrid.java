4aa70837ccc25914ed6cfec802e43084e26d6e7d
Connor O'Shaughnessy <connor.oshaughnessy@version1.com>
2023-07-07 12:23:46 +0100

    Extension attempt

--- RobotGrid.java
+++ RobotGrid.java
@@ -32,40 +32,81 @@ public class RobotGrid {
         boolean[][] thousandbythousand = new boolean[1000][1000];
 
 
-        queue.offer(new Coord(0,0));
+        queue.offer(new Coord(0, 0));
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             safeSquares++;
             Coord curr = queue.poll();
             thousandbythousand[curr.x][curr.y] = true;
-            if(!thousandbythousand[curr.x + 1][curr.y] && isSafe(curr.x+1,curr.y) ){queue.offer(new Coord(curr.x+1,curr.y));}
-            if((curr.x-1 > 0) && !thousandbythousand[curr.x - 1][curr.y ] && isSafe(curr.x-1,curr.y)){queue.offer(new Coord(curr.x-1,curr.y));}
-            if(!thousandbythousand[curr.x][curr.y + 1] && isSafe(curr.x,curr.y+1)){queue.offer(new Coord(curr.x,curr.y+1));}
-            if((curr.y-1 > 0) && !thousandbythousand[curr.x][curr.y - 1] && isSafe(curr.x,curr.y-1)){queue.offer(new Coord(curr.x,curr.y-1));}
+            if (!thousandbythousand[curr.x + 1][curr.y] && isSafe(curr.x + 1, curr.y)) {
+                queue.offer(new Coord(curr.x + 1, curr.y));
+            }
+            if ((curr.x - 1 > 0) && !thousandbythousand[curr.x - 1][curr.y] && isSafe(curr.x - 1, curr.y)) {
+                queue.offer(new Coord(curr.x - 1, curr.y));
+            }
+            if (!thousandbythousand[curr.x][curr.y + 1] && isSafe(curr.x, curr.y + 1)) {
+                queue.offer(new Coord(curr.x, curr.y + 1));
+            }
+            if ((curr.y - 1 > 0) && !thousandbythousand[curr.x][curr.y - 1] && isSafe(curr.x, curr.y - 1)) {
+                queue.offer(new Coord(curr.x, curr.y - 1));
+            }
         }
+
+
 
         System.out.println();
         return safeSquares * 4;
     }
 
     public int shortestSafeJourney(int a, int b, int x, int y) {
-        // TODO: Add implementation logic here
-        throw new UnsupportedOperationException("Not implemented yet");
+        int traveled = 0;
+        PriorityQueue<Coord> queue = new PriorityQueue<>();
+        boolean[][] thousandbythousand = new boolean[1000][1000];
+        boolean finished = false;
+
+        while (!queue.isEmpty() || !finished) {
+            traveled++;
+            Coord curr = queue.poll();
+            thousandbythousand[curr.x][curr.y] = true;
+            if (!thousandbythousand[curr.x + 1][curr.y] && isSafe(curr.x + 1, curr.y)) {
+                queue.offer(new Coord(curr.x + 1, curr.y,traveled));
+            }
+            if ((curr.x - 1 > 0) && !thousandbythousand[curr.x - 1][curr.y] && isSafe(curr.x - 1, curr.y)) {
+                queue.offer(new Coord(curr.x - 1, curr.y,traveled));
+            }
+            if (!thousandbythousand[curr.x][curr.y + 1] && isSafe(curr.x, curr.y + 1)) {
+                queue.offer(new Coord(curr.x, curr.y + 1,traveled));
+            }
+            if ((curr.y - 1 > 0) && !thousandbythousand[curr.x][curr.y - 1] && isSafe(curr.x, curr.y - 1)) {
+                queue.offer(new Coord(curr.x, curr.y - 1,traveled));
+            }
+        }
+
+        return 0;
     }
 
-    class Coord implements Comparable{
+    class Coord implements Comparable<Coord>{
 
         final int x;
         final int y;
+        final int traveled;
 
         @Override
-        public int compareTo(Object T){
-            return 0;
+        public int compareTo(Coord T){
+            if( this.traveled > T.traveled){return 1;}
+            else if(this.traveled < T.traveled){return -1;}
+            else{return 0;}
         }
         
         public Coord(int x, int y){
             this.x = x;
             this.y = y;
+            this.traveled = 0;
+        }
+        public Coord(int x, int y,int traveled){
+            this.x = x;
+            this.y = y;
+            this.traveled = traveled;
         }
     }
 
